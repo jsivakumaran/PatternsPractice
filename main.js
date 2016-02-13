925f92e938b7582bcba9ba07f67527a7b5e49824
Jay Sivakumaran <jsivakumaran@outlook.com>
2016-02-12 21:03:09 -0800

    refactor the code into modules using the node.js common.js require statements

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-
-
-var Task = function(name){
-  this.name = name;
-  this.completed = false;
-}
-
-Task.prototype.complete = function(){
-  console.log('completing task: ' + this.name);
-  this.completed = true;
-}
-
-Task.prototype.save = function(){
-  console.log('saving Task: ' + this.name);
-}
+var Task = require('./task');
 
 var task1 = new Task('create a demo for constructors');
 var task2 = new Task('create a demo for modules');
