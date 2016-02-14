2b5b58db1dcd3ad9596064bccadf1851936c4a50
Jay Sivakumaran <jsivakumaran@outlook.com>
2016-02-13 17:05:30 -0800

    refactor taskRepository.js to make use of the revealing module pattern - basically a documenting api that lets the developer know which functions are available to you by looking a the return statement without having to read through the entire code

--- Module/taskRepository.js
+++ Module/taskRepository.js
@@ -1,17 +1,24 @@
 
 var repo = function(){
-  return{
-    get: function(id){
+
+    var db = {};
+
+    var get = function(id){
       console.log('Getting task ' + id);
       return {
         name: 'new task from db'
       }
-    },
-    save: function(task){
+    }
+    var save = function(task){
       console.log('Saving ' + task.name + ' to the db')
+    }
+
+    return {
+      get: get,
+      save: save
     }
   }
 
-}
+
 
 module.exports = repo();
