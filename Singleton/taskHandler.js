53a1193c2042cc01e456d979a0af1ae6e270472e
Jay Sivakumaran <jsivakumaran@outlook.com>
2016-02-15 20:04:06 -0800

    before singleton, notice how there are two newing up task repo showing up when running node main.js

---
+++ Singleton/taskHandler.js
@@ -0,0 +1,14 @@
+
+var repo = require('./repo');
+
+var myrepo = repo();
+
+var taskHandler = function(){
+  return{
+    save: function(){
+      myrepo.save('Hi from taskHandler');
+    }
+  }
+}
+
+module.exports = taskHandler();
